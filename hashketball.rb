75f010714d6343d139d9e587c517ae9dc7b6c00d
Kevin Downs <kevin.downs04@gmail.com>
2019-10-31 05:58:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,102 @@
-# Write your code here!
+require "pry"
 
+def game_hash
+  game_hash = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [
+        {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, 
+         :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1},
+        {:player_name => "Reggie Evans", :number => 30, :shoe => 14, :points => 12, 
+         :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7},
+        {:player_name => "Brook Lopez", :number => 11, :shoe => 17, :points => 17, 
+         :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15},
+        {:player_name => "Mason Plumlee", :number => 1, :shoe => 19, :points => 26, 
+        :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5},
+        {:player_name => "Jason Terry", :number => 31, :shoe => 15, :points => 19, 
+        :rebounds => 2, :assists => 2, :steals => 4, :blocks => 11, :slam_dunks => 1}
+      ]
+    }, 
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Purple, Turquoise"],
+      :players => [
+        {:player_name => "Jeff Adrien", :number => 4, :shoe => 18, :points => 10, 
+         :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2},
+        {:player_name => "Bismack Biyombo", :number => 0, :shoe => 16, :points => 12, 
+         :rebounds => 4, :assists => 7, :steals => 22, :blocks => 15, :slam_dunks => 10},
+        {:player_name => "DeSagna Diop", :number => 2, :shoe => 14, :points => 24, 
+         :rebounds => 12, :assists => 12, :steals => 4, :blocks => 5, :slam_dunks => 5},
+        {:player_name => "Ben Gordon", :number => 8, :shoe => 15, :points => 33, 
+         :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_dunks => 0},
+        {:player_name => "Kemba Walker", :number => 33, :shoe => 15, :points => 6, 
+         :rebounds => 12, :assists => 12, :steals => 7, :blocks => 5, :slam_dunks => 12}
+      ]
+    }
+  }
+end
+
+def find_player_value(attribute, player_name)
+  game_hash.each do |key, value|
+    value[:players].each do |player_hash|
+      return player_hash[attribute] if player_hash[:player_name] == player_name
+    end
+  end
+end
+
+def num_points_scored(player_name)
+  find_player_value(:points, player_name)
+end
+
+def shoe_size(player_name)
+  find_player_value(:shoe, player_name)
+end
+
+def team_colors(team_name)
+#return array of team colors
+  game_hash.each do |key, value|
+    if value[:team_name] == team_name
+      return value[:colors]
+    end
+  end
+end
+
+def team_names
+#return array of team names
+end
+
+def player_numbers(team_name)
+#returns array of jersey numbers
+end
+
+def player_stats(player_name)
+#returns hash of player's stats 
+end
+
+def big_shoe_rebounds
+# returns number of rebounds of player with largest shoe size
+end
+
+#BONUS QUESTIONS
+
+def most_points_scored
+#returns player with most points 
+end
+
+def winning_team
+#returns team with most points scored
+end
+
+def player_with_longest_name
+#returns player with longest name 
+end
+
+#SUPER BONUS
+
+def long_name_steals_a_ton?
+#returns true is plaayer with the longest name had the most steals 
+end
 
 
 
